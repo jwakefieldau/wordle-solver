bc75cbab9cb59f948f5f82db926517d7774d21b6
James Wakefield <jmw1982@gmail.com>
2022-01-15 21:53:57 +1100

    progress commit - working on analysis

--- wordle/main.py
+++ wordle/main.py
@@ -1,12 +1,49 @@
 from string import ascii_lowercase
 
+MIN_WORD_LEN = 2
+
 class WordleSolver(object):
 
     def __init__(self, dict_file_path, word_len):
-        if word_len < 2:
-            raise ValueError('minimum word_len == 2')
+        if word_len < MIN_WORD_LEN:
+            raise ValueError(f'minimum word_len == {MIN_WORD_LEN}')
         else:
             self.word_len = word_len
+
+        self.dict_file_path = dict_file_path
+        
+        # overall word set
+        self.word_set = set()
+
+        self._load_dict()
+        self._init_analysis()
+        self._dict_analysis_stats()
+
+        self.reset_game()
+
+    # load dictionary, filter for length
+    # relevant packages for ubuntu:
+    # wamerican, wbritish, wcanadian
+    # *-large | *-huge | *-insane | *-small if defaults don't work
+
+    # currently just using /usr/share/dict/british-english
+
+    def _load_dict(self):
+        with open(self.dict_file_path, 'rt') as f:
+            for word in f:
+                word = word.strip()
+                skip_word = False
+                if len(word) == self.word_len:
+                    for c in word:
+                        if c not in ascii_lowercase:
+                            skip_word = True
+                            break
+                    if not skip_word:
+                        self.word_set.add(word)
+
+
+    # initialise all the analysis data structures
+    def _init_analysis(self):
 
         # in how many words does letter l occur n times?
         self.letter_freq_dl = dict()
@@ -29,7 +66,7 @@ class WordleSolver(object):
                 self.letter_pos_freq_ld[i][c] = 0
 
         # in how many words does digram dg appear at position i?
-        self.digram_pos_freq_ld = list()
+        self.digram_pos_freq_ld = [dict()] * self.word_len
         for i in range(self.word_len - 1):
             self.digram_pos_freq_ld[i] = dict()
             for c1 in ascii_lowercase:
@@ -37,36 +74,22 @@ class WordleSolver(object):
                     dg = f'{c1}{c2}'
                     self.digram_pos_freq_ld[i][dg] = 0
 
-        # overall word set
-        self.word_set = set()
 
         # set of words with letter l at position p
-        self.word_set_per_letter_per_pos = list()
+        self.word_set_per_letter_per_pos = [dict()] * self.word_len
         for i in range(self.word_len):
             self.word_set_per_letter_per_pos[i] = dict()
             for c in ascii_lowercase:
                 self.word_set_per_letter_per_pos[i][c] = set()
-        
-        self.dict_file_path = dict_file_path
-
-    # load dictionary, filter for length
-    # relevant packages for ubuntu:
-    # wamerican, wbritish, wcanadian
-    # *-large | *-huge | *-insane | *-small if defaults don't work
-
-    # currently just using wamerican - /usr/share/dict/words
-
-    def _load_dict(self):
-        with open(self.dict_file_path, 'rt') as f:
-            for word in self.dict_file_path:
-                if len(word) == self.word_len:
-                    self.word_set.add(word)
+         
 
     # freq analyse letters at each position, maybe digrams too?
     # freq analyse counts of letters in word, eg: letter 'e' appears
     # x times in y words, same with digrams
     #TODO - if this analysis is expensive enough, write something to save it to disk
-    def _analyse_dict(self):
+
+    #TODO - sort frequency stats
+    def _dict_analysis_stats(self):
         for word in self.word_set:
             for (i, c,) in enumerate(word):
 
@@ -99,20 +122,47 @@ class WordleSolver(object):
                 self.digram_freq_dl[dg][cur_dg_count] += 1
 
 
+    def _score_words(self):
+        #TODO - use sorted frequency stats to score each word and then sort the
+        # set by score
+
+
     def reset_game(self):
-        #TODO - reset game state
+        self.game_word_set = set(self.word_set)
 
 
-    # answer contains c at index i
+    # c must be present at position i, prune any words
+    # that have any other letter there
     def add_green(self, i, c):
-        #TODO - change game state
+        prune_set = set()
+        for word in self.game_word_set:
+            if word[i] != c:
+                prune_set.add(word)
 
+        self.game_word_set -= prune_set     
 
-    # answer contains c, but NOT at index i
+    
+    # c must be present, but NOT at position i, prune
+    # any words that have c at position i, and any words
+    # that do not have c at all
     def add_yellow(self, i, c):
-        #TODO - change game state
+        prune_set = set()
+        for word in self.game_word_set:
+            if word[i] == c:
+                prune_set.add(word)
+            elif c not in word:
+                prune_set.add(word)
+
+        self.game_word_set -= prune_set
 
 
-    # answer does NOT contain c
+    # c is not present at all, prune any words that contain c
     def add_grey(self, c):
-        #TODO - change game state
+        prune_set = set()
+        for word in self.game_word_set:
+            if c in word:
+                prune_set.add(word)
+
+        self.game_word_set -= prune_set
+
+
